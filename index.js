a1c2fc7541767f51ea46e0ff97a382b4e06a62a4
Zane McCaig <zane@instanews.com>
2016-06-02 15:05:23 -0400

    Connected the database and plotted views fullscreen

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+var Db = require('mongodb').Db,
+    Server = require('mongodb').Server,
+    assert = require('assert'),
+    MongoClient = require('mongodb').MongoClient;
+
+
+var Articles, Shares, Views, Clicks, UpVotes, DownVotes;
+
+var url = 'mongodb://localhost:27017/interactions';
+MongoClient.connect(url, function(err, db) {
+  // Create a test collection
+  db.collection('view', function(err, collection) {
+    Views = collection;
+    db.collection('click', function(err, collection) {
+      Clicks = collection;
+      db.collection('upVote', function(err, collection) {
+        UpVotes = collection;
+        db.collection('downVote', function(err, collection) {
+          DownVotes = collection;
+          db.collection('share', function(err, collection) {
+            Shares = collection;
+          });
+        });
+      });
+    });
+  });
+});
+
 
 var express = require('express');
 var app = express();
@@ -5,7 +33,21 @@ var app = express();
 app.use(express.static('public'));
 app.use('/components', express.static('node_modules'));
 
-app.use('/api/articles', function(req, res) {
+app.get('/api/views', function(req, res) {
+  try {
+    console.log(req.query);
+    var query = JSON.parse(req.query.query);
+    console.log(query);
+    Views.find(query).toArray(function(err, views) {
+      res.send(views);
+    });
+  } catch(e) {
+    console.log(e);
+    res.send(e);
+  }
+});
+
+app.get('/api/articles', function(req, res) {
   //TODO Connect to mongoDB and query that shit
   var data = [
     { lat: 54.70831, lng: -97.871324},
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -22,16 +22,26 @@ app.service('Server', [
     $resource
   ){
     return {
+      Views: $resource('/api/views'),
       Articles: $resource('/api/articles/:id')
     };
 }]);
 
+app.constant(
+  '_',
+  window._
+);
+
 app.controller('mainCtrl', [
   '$scope',
+  '$timeout',
   'Server',
+  '_',
   function(
     $scope,
-    Server
+    $timeout,
+    Server,
+    _
   ) {
     var element = document.getElementById('map');
     var options = {
@@ -41,19 +51,93 @@ app.controller('mainCtrl', [
 
     var map = new google.maps.Map(element, options);
 
-    var articles = Server.Articles.query(function() {
-      var data = [];
-      for(var i in articles) {
-        if(articles[i].lat && articles[i].lng) {
-          data.push(new google.maps.LatLng(articles[i].lat, articles[i].lng));
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+      if(places.length === 0) {
+        return;
+      }
+
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if(place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+
+    var posToLatLng = function(pos) {
+      if(pos.lat && pos.lng) {
+        return new google.maps.LatLng(pos.lat, pos.lng);
+      } else if(pos.coordinates) {
+        return new google.maps.LatLng(pos.coordinates[1], pos.coordinates[0]);
+      }
+    };
+
+    $scope.noLocation = 0;
+
+    var excludedUsers = [
+      'zane',
+      'shawn',
+      'superman',
+      'flash',
+      'ben',
+      'fastresponse',
+      '3000',
+      'mrreportz'
+    ];
+    //excludedUsers = [];
+
+    var views = Server.Views.query({
+      query: {
+        username: {
+          $nin: excludedUsers 
         }
       }
+    },function() {
+      var data = [];
+      for(var i in views) {
+        if(views[i].location) {
+          var loc = posToLatLng(views[i].location);
+          if(loc) {
+            data.push(loc);
+          } else {
+            $scope.noLocation++;
+          }
+        } else {
+          $scope.noLocation++;
+        }
+      }
+
+      $scope.total = views.length;
 
       var heatmap = new google.maps.visualization.HeatmapLayer({
         map: map,
         radius: 15,
+        opacity: 1,
+        dissipating: true,
         data: data
       });
+
+      google.maps.event.addListener(map, 'bounds_changed', _.debounce(function() {
+        var bnds = map.getBounds();
+        var minimized = [];
+        for( var i in data) {
+          if(bnds.contains(data[i])){
+            minimized.push(data[i]);
+          }
+        }
+        $timeout(function() {
+          $scope.total = minimized.length;
+        });
+        heatmap.setData(minimized);
+      },500));
     });
 
 }]);
